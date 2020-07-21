a5cf3188f4613acde7d02ae48c56ba1e8b214b2d
wangkai <wk.dong.up@gmail.com>
2020-07-21 22:08:05 +0800

    删除不必要的QRcode

--- src/index.tsx
+++ src/index.tsx
@@ -11,22 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-window.onload = function () {
-  setTimeout(function () {
-    window.scroll(0, 200);
-  }, 0);
-};
-if(document.documentElement.clientWidth > 500){
-  window.alert('请扫描二维码，保证体验效果')
-  const img = document.createElement('img')
-  img.src = './QRcode.png'
-  img.style.position = 'fixed'
-  img.style.left = '50%'
-  img.style.top = '50%'
-  img.style.transform = 'translate(-50%,-50%)'
-  img.style.boxShadow = '0 10px 200px rgba(0,0,0,0.3)'
-  document.body.appendChild(img)
-}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
